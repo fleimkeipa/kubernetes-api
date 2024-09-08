85925603390ae2e081fbba48048870cf854f3e81
fleimkeipa <adem_sahin12@hotmail.com>
2024-09-09 01:04:52 +0300

    Add controller handler and uc funcs

---
+++ controller/deployment.go
@@ -0,0 +1,84 @@
+package controller
+
+import (
+	"net/http"
+
+	"kub/model"
+	"kub/uc"
+
+	"github.com/labstack/echo/v4"
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type DeploymentHandler struct {
+	deploymentUC *uc.DeploymentUC
+}
+
+func NewDeploymentHandler(podsUC *uc.DeploymentUC) *DeploymentHandler {
+	return &DeploymentHandler{
+		deploymentUC: podsUC,
+	}
+}
+
+func (rc *DeploymentHandler) Create(c echo.Context) error {
+	var input model.PodsRequest
+
+	if err := c.Bind(&input); err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
+	}
+
+	var opts = metav1.CreateOptions{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "",
+			APIVersion: "",
+		},
+		DryRun:          []string{},
+		FieldManager:    "",
+		FieldValidation: "",
+	}
+	var deployment = v1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       input.TypeMeta.Kind,
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      input.ObjectMeta.Name,
+			Namespace: input.ObjectMeta.NameSpace,
+		},
+	}
+
+	_, err := rc.deploymentUC.Create(c.Request().Context(), &deployment, opts)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusCreated, echo.Map{"deployment": deployment.Name})
+}
+
+func (rc *DeploymentHandler) List(c echo.Context) error {
+	var namespace = c.QueryParam("namespace")
+
+	var opts = metav1.ListOptions{}
+
+	list, err := rc.deploymentUC.Get(c.Request().Context(), namespace, opts)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{"data": list})
+}
+
+func (rc *DeploymentHandler) GetByNameOrUID(c echo.Context) error {
+	var namespace = c.QueryParam("namespace")
+	var nameOrUID = c.Param("id")
+
+	var opts = metav1.ListOptions{}
+
+	list, err := rc.deploymentUC.GetByNameOrUID(c.Request().Context(), namespace, nameOrUID, opts)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{"data": list})
+}
---
+++ uc/deployment.go
@@ -0,0 +1,64 @@
+package uc
+
+import (
+	"context"
+
+	"kub/repositories/interfaces"
+
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+type DeploymentUC struct {
+	deploymentRepo interfaces.DeploymentInterfaces
+}
+
+func NewDeploymentUC(deploymentRepo interfaces.DeploymentInterfaces) *DeploymentUC {
+	return &DeploymentUC{
+		deploymentRepo: deploymentRepo,
+	}
+}
+
+func (rc *DeploymentUC) Get(ctx context.Context, namespace string, opts metav1.ListOptions) (*v1.DeploymentList, error) {
+	opts.TypeMeta.Kind = "deployment"
+	if namespace == "" {
+		namespace = "default"
+	}
+
+	return rc.deploymentRepo.Get(ctx, namespace, opts)
+}
+
+func (rc *DeploymentUC) Create(ctx context.Context, deployment *v1.Deployment, opts metav1.CreateOptions) (*v1.Deployment, error) {
+	deployment.TypeMeta.Kind = "deployment"
+	if deployment.ObjectMeta.Namespace == "" {
+		deployment.ObjectMeta.Namespace = "default"
+	}
+
+	return rc.deploymentRepo.Create(ctx, deployment, opts)
+}
+
+func (rc *DeploymentUC) GetByNameOrUID(ctx context.Context, namespace, nameOrUID string, opts metav1.ListOptions) (*v1.Deployment, error) {
+	opts.TypeMeta.Kind = "deployment"
+	if namespace == "" {
+		namespace = "default"
+	}
+
+	opts.Limit = 100
+	deployments, err := rc.deploymentRepo.Get(ctx, namespace, opts)
+	if err != nil {
+		return nil, err
+	}
+	for _, v := range deployments.Items {
+		if v.Name == nameOrUID || v.UID == types.UID(nameOrUID) {
+			return &v, nil
+		}
+	}
+
+	if deployments.ListMeta.Continue == "" {
+		return &v1.Deployment{}, nil
+	}
+
+	opts.Continue = deployments.ListMeta.Continue
+	return rc.GetByNameOrUID(ctx, namespace, nameOrUID, opts)
+}
