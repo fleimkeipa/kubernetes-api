a59a978977b602201854f905cf2413dee218c71f
fleimkeipa <adem_sahin12@hotmail.com>
2024-09-10 12:34:27 +0300

    Add namespace update repo func

--- repositories/interfaces/namespaces.go
+++ repositories/interfaces/namespaces.go
@@ -9,6 +9,7 @@ import (
 
 type NamespaceInterfaces interface {
 	Get(context.Context, metav1.ListOptions) (*corev1.NamespaceList, error)
-	Create(ctx context.Context, namespace *corev1.Namespace, opts metav1.CreateOptions) (*corev1.Namespace, error)
+	Create(context.Context, *corev1.Namespace, metav1.CreateOptions) (*corev1.Namespace, error)
 	Delete(context.Context, string, metav1.DeleteOptions) error
+	Update(context.Context, *corev1.Namespace, metav1.UpdateOptions) (*corev1.Namespace, error)
 }
