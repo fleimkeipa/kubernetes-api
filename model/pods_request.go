d55419892fc1386fd7b44fca2aa414fa38e2e052
fleimkeipa <adem_sahin12@hotmail.com>
2024-09-15 21:50:38 +0300

    Add pod crud request struc

---
+++ model/pods_request.go
@@ -0,0 +1,34 @@
+package model
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+type (
+	PodsCreateRequest struct {
+		Pod  Pod                  `json:"pod"`
+		Opts metav1.CreateOptions `json:"opts"`
+	}
+)
+
+type (
+	PodsUpdateRequest struct {
+		Pod  PodUpdate            `json:"pod"`
+		Opts metav1.UpdateOptions `json:"opts"`
+	}
+
+	PodUpdate struct {
+		Spec SpecRequest `json:"spec,omitempty"`
+	}
+
+	SpecRequest struct {
+		InitContainers                []ContainerRequest `json:"initContainers,omitempty"`
+		Containers                    []ContainerRequest `json:"containers"`
+		ActiveDeadlineSeconds         *int64             `json:"activeDeadlineSeconds,omitempty"`
+		TerminationGracePeriodSeconds *int64             `json:"terminationGracePeriodSeconds,omitempty"`
+		Tolerations                   []Toleration       `json:"tolerations,omitempty"`
+	}
+
+	ContainerRequest struct {
+		Name  string `json:"name"`
+		Image string `json:"image,omitempty"`
+	}
+)
