534c3172f85291d9def763f5c7abae000d5e2034
fleimkeipa <adem_sahin12@hotmail.com>
2024-09-15 21:51:12 +0300

    Update tests

--- uc/pods_test.go
+++ uc/pods_test.go
@@ -10,10 +10,28 @@ import (
 	"github.com/fleimkeipa/kubernetes-api/repositories"
 	"github.com/fleimkeipa/kubernetes-api/repositories/interfaces"
 
+	"github.com/go-pg/pg"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
+
+func init() {
+	loadEnv()
+}
+
+func initTestKubernetes() *kubernetes.Clientset {
+	client, err := pkg.NewKubernetesClient()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return client
+}
+
+func initTestDB() *pg.DB {
+	return pkg.NewPSQLClient()
+}
 
 func TestPodsUC_GetByName(t *testing.T) {
 	type fields struct {
@@ -64,23 +82,17 @@ func TestPodsUC_GetByName(t *testing.T) {
 	}
 }
 
-func initTestKubernetes() *kubernetes.Clientset {
-	client, err := pkg.NewKubernetesClient()
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return client
-}
-
 func TestPodsUC_Update(t *testing.T) {
+	var second int64 = 128
 	type fields struct {
-		podsRepo interfaces.PodsInterfaces
+		podsRepo  interfaces.PodsInterfaces
+		eventRepo interfaces.EventsInterfaces
 	}
 	type args struct {
 		ctx  context.Context
 		pod  *model.Pod
 		opts metav1.UpdateOptions
+		id   string
 	}
 	tests := []struct {
 		name    string
@@ -92,12 +104,29 @@ func TestPodsUC_Update(t *testing.T) {
 		{
 			name: "",
 			fields: fields{
-				podsRepo: repositories.NewPodsRepository(initTestKubernetes()),
+				podsRepo:  repositories.NewPodsRepository(initTestKubernetes()),
+				eventRepo: repositories.NewEventRepository(initTestDB()),
 			},
 			args: args{
-				ctx:  context.TODO(),
-				pod:  &model.Pod{},
+				ctx: context.TODO(),
+				pod: &model.Pod{
+					ObjectMeta: model.ObjectMeta{
+						Name:      "pod1",
+						Namespace: "default",
+					},
+					Spec: model.PodSpec{
+						Containers: []model.Container{
+							{
+								Name:       "nginx",
+								Image:      "nginx:1.16",
+								WorkingDir: "dir1",
+							},
+						},
+						ActiveDeadlineSeconds: &second,
+					},
+				},
 				opts: metav1.UpdateOptions{},
+				id:   "pod1",
 			},
 			want:    &corev1.Pod{},
 			wantErr: false,
@@ -106,9 +135,10 @@ func TestPodsUC_Update(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			rc := &PodsUC{
-				podsRepo: tt.fields.podsRepo,
+				podsRepo:  tt.fields.podsRepo,
+				eventRepo: tt.fields.eventRepo,
 			}
-			got, err := rc.Update(tt.args.ctx, tt.args.pod, tt.args.opts)
+			got, err := rc.Update(tt.args.ctx, tt.args.id, tt.args.pod, tt.args.opts)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("PodsUC.Update() error = %v, wantErr %v", err, tt.wantErr)
 				return
