37cd8092b221f15775607d5bbb14103352a1dafc
fleimkeipa <adem_sahin12@hotmail.com>
2024-09-16 15:18:03 +0300

    Delete redundant model

--- model/role.go
+++ model/role.go
@@ -5,10 +5,3 @@ const (
 	ViewerRole = 5
 	EditorRole = 1
 )
-
-// Role model
-type Role struct {
-	ID          string `bson:"_id,omitempty"` // MongoDB ObjectID, primary key
-	Name        string `bson:"name" json:"name"`
-	Description string `bson:"description" json:"description"`
-}
