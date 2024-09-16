d61e2b009cc6c291e8bfecad227eba85e99a6bbd
fleimkeipa <adem_sahin12@hotmail.com>
2024-09-17 01:38:53 +0300

    Add field opts

--- model/pagination.go
+++ model/pagination.go
@@ -1,11 +1,15 @@
 package model
 
 type PaginationOpts struct {
-	Skip  uint
-	Limit uint
+	Skip  int
+	Limit int
 }
 
 type Filter struct {
 	IsSended bool
 	Value    string
+}
+
+type FieldsOpts struct {
+	Fields []string
 }
