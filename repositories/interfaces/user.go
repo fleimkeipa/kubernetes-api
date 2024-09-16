d376d5439576881e86ef5dda5f8ac82a695e9539
fleimkeipa <adem_sahin12@hotmail.com>
2024-09-17 01:39:31 +0300

    Add user fields control

--- repositories/interfaces/user.go
+++ repositories/interfaces/user.go
@@ -9,6 +9,7 @@ import (
 type UserInterfaces interface {
 	Create(context.Context, model.User) (*model.User, error)
 	Update(context.Context, model.User) (*model.User, error)
+	List(context.Context, *model.UserFindOpts) ([]model.User, error)
 	GetByUsernameOrEmail(context.Context, string) (*model.User, error)
 	GetByID(context.Context, string) (*model.User, error)
 }
--- repositories/user.go
+++ repositories/user.go
@@ -40,9 +40,11 @@ func (rc *UserRepository) Update(ctx context.Context, user model.User) (*model.U
 func (rc *UserRepository) GetByID(ctx context.Context, id string) (*model.User, error) {
 	var user = new(model.User)
 
+	var fields = []string{}
 	err := rc.db.
 		Model(user).
 		Where("id = ?", id).
+		Column(fields...).
 		Select()
 	if err != nil {
 		return nil, fmt.Errorf("failed to find user [%s] id, error: %w", id, err)
@@ -63,4 +65,67 @@ func (rc *UserRepository) GetByUsernameOrEmail(ctx context.Context, usernameOrEm
 	}
 
 	return user, nil
+}
+
+func (rc *UserRepository) List(ctx context.Context, opts *model.UserFindOpts) ([]model.User, error) {
+	var users = make([]model.User, 0)
+	var filter = rc.fillFilter(opts)
+	var fields = rc.fillFields(opts)
+	if filter == "" {
+		err := rc.db.
+			Model(&users).
+			Column(fields...).
+			Limit(opts.Limit).
+			Offset(opts.Skip).
+			Select()
+		if err != nil {
+			return nil, err
+		}
+
+		return users, nil
+	}
+
+	err := rc.db.
+		Model(&users).
+		Column(fields...).
+		Where(filter).
+		Limit(opts.Limit).
+		Offset(opts.Skip).
+		Select()
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (rc *UserRepository) fillFields(opts *model.UserFindOpts) []string {
+	if len(opts.Fields) == 0 {
+		return []string{}
+	}
+
+	if len(opts.Fields) == 1 {
+		if opts.Fields[0] == model.ZeroCreds {
+			return []string{
+				"id", "username", "email", "role_id", "deleted_at",
+			}
+		}
+	}
+
+	return opts.Fields
+}
+
+func (rc *UserRepository) fillFilter(opts *model.UserFindOpts) string {
+	var filter string
+	if opts.Username.IsSended {
+		filter = addInFilter(filter, "username", opts.Username.Value)
+	}
+	if opts.Email.IsSended {
+		filter = addInFilter(filter, "email", opts.Email.Value)
+	}
+	if opts.RoleID.IsSended {
+		filter = addInFilter(filter, "role_id", opts.RoleID.Value)
+	}
+
+	return filter
 }
