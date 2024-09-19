2d7d3f9f94ec65aaaad74666ffa632dc3c833e85
fleimkeipa <adem_sahin12@hotmail.com>
2024-09-20 00:19:18 +0300

    Update namespace requests

--- model/namespace_request.go
+++ model/namespace_request.go
@@ -1,16 +1,15 @@
 package model
 
 import (
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type NamespaceCreateRequest struct {
-	Namespace corev1.Namespace     `json:"namespace"`
+	Namespace Namespace            `json:"namespace"`
 	Opts      metav1.CreateOptions `json:"opts"`
 }
 
 type NamespaceUpdateRequest struct {
-	Namespace corev1.Namespace     `json:"namespace"`
+	Namespace Namespace            `json:"namespace"`
 	Opts      metav1.UpdateOptions `json:"opts"`
 }
