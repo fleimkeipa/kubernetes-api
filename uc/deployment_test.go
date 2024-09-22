2cea030d430fe44c0130dd42216d02b8d39fd66f
fleimkeipa <adem_sahin12@hotmail.com>
2024-09-22 23:47:03 +0300

    Add test for deployment GetByNameOrUID func

--- uc/deployment_test.go
+++ uc/deployment_test.go
@@ -67,3 +67,54 @@ func TestDeploymentUC_List(t *testing.T) {
 	}
 }
 
+func TestDeploymentUC_GetByNameOrUID(t *testing.T) {
+	type fields struct {
+		deploymentRepo interfaces.DeploymentInterfaces
+		eventUC        *EventUC
+	}
+	type args struct {
+		ctx       context.Context
+		namespace string
+		nameOrUID string
+		opts      metav1.ListOptions
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *v1.Deployment
+		wantErr bool
+	}{
+		{
+			name: "",
+			fields: fields{
+				deploymentRepo: deploymentTestRepo,
+				eventUC:        &EventUC{},
+			},
+			args: args{
+				ctx:       context.TODO(),
+				namespace: "",
+				nameOrUID: "",
+				opts:      metav1.ListOptions{},
+			},
+			want:    &v1.Deployment{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rc := &DeploymentUC{
+				deploymentRepo: tt.fields.deploymentRepo,
+				eventUC:        tt.fields.eventUC,
+			}
+			got, err := rc.GetByNameOrUID(tt.args.ctx, tt.args.namespace, tt.args.nameOrUID, tt.args.opts)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DeploymentUC.GetByNameOrUID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DeploymentUC.GetByNameOrUID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
