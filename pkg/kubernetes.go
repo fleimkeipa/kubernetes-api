23f4a52f7c87c1b781946a2921f6fb43e03347ae
fleimkeipa <adem_sahin12@hotmail.com>
2024-10-06 00:04:34 +0300

    Update kubernetes init for config

--- pkg/kubernetes.go
+++ pkg/kubernetes.go
@@ -2,29 +2,44 @@ package pkg
 
 import (
 	"flag"
+	"os"
 	"path/filepath"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
 )
 
 func NewKubernetesClient() (*kubernetes.Clientset, error) {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	kubeconfigPath := ""
+
+	// Use the KUBECONFIG environment variable if it is set
+	if envKubeconfig := os.Getenv("KUBECONFIG"); envKubeconfig != "" {
+		kubeconfigPath = envKubeconfig
 	}
+
+	// Use the default kubeconfig path if not set
+	if kubeconfigPath == "" {
+		if os.Geteuid() == 0 {
+			kubeconfigPath = "/root/.kube/config"
+		} else {
+			homeDir := os.Getenv("HOME")
+			if homeDir != "" {
+				kubeconfigPath = filepath.Join(homeDir, ".kube", "config")
+			}
+		}
+	}
+
+	// Let the user override the path with the --kubeconfig flag
+	kubeconfig := flag.String("kubeconfig", kubeconfigPath, "absolute path to the kubeconfig file")
 	flag.Parse()
 
-	// use the current context in kubeconfig
+	// Build the client configuration
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		return nil, err
 	}
 
-	// create the clientset
+	// Create the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		return nil, err
