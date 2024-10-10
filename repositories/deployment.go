09f0eb18216c551cf4c4e05afdf0b118b42f19b2
fleimkeipa <adem_sahin12@hotmail.com>
2024-10-11 01:43:30 +0300

    Update deployment options

--- repositories/deployment.go
+++ repositories/deployment.go
@@ -6,7 +6,6 @@ import (
 	"github.com/fleimkeipa/kubernetes-api/model"
 
 	v1 "k8s.io/api/apps/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -19,74 +18,25 @@ func NewDeploymentInterfaces(client *kubernetes.Clientset) *DeploymentInterfaces
 }
 
 func (rc *DeploymentInterfaces) Create(ctx context.Context, deployment *v1.Deployment, opts model.CreateOptions) (*v1.Deployment, error) {
-	metaOpts := metav1.CreateOptions{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       opts.TypeMeta.Kind,
-			APIVersion: opts.TypeMeta.APIVersion,
-		},
-		DryRun:          opts.DryRun,
-		FieldManager:    opts.FieldManager,
-		FieldValidation: opts.FieldValidation,
-	}
+	metaOpts := convertCreateOptsToKube(opts)
+
 	return rc.client.AppsV1().Deployments(deployment.Namespace).Create(ctx, deployment, metaOpts)
 }
 
 func (rc *DeploymentInterfaces) Update(ctx context.Context, deployment *v1.Deployment, opts model.UpdateOptions) (*v1.Deployment, error) {
-	metaOpts := metav1.UpdateOptions{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       opts.TypeMeta.Kind,
-			APIVersion: opts.TypeMeta.APIVersion,
-		},
-		DryRun:          opts.DryRun,
-		FieldManager:    opts.FieldManager,
-		FieldValidation: opts.FieldValidation,
-	}
+	metaOpts := convertUpdateOptsToKube(opts)
+
 	return rc.client.AppsV1().Deployments(deployment.Namespace).Update(ctx, deployment, metaOpts)
 }
 
 func (rc *DeploymentInterfaces) List(ctx context.Context, namespace string, opts model.ListOptions) (*v1.DeploymentList, error) {
-	metaOpts := metav1.ListOptions{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       opts.TypeMeta.Kind,
-			APIVersion: opts.TypeMeta.APIVersion,
-		},
-		LabelSelector:        opts.LabelSelector,
-		FieldSelector:        opts.FieldSelector,
-		Watch:                opts.Watch,
-		AllowWatchBookmarks:  opts.AllowWatchBookmarks,
-		ResourceVersion:      opts.ResourceVersion,
-		ResourceVersionMatch: metav1.ResourceVersionMatch(opts.ResourceVersionMatch),
-		TimeoutSeconds:       opts.TimeoutSeconds,
-		Limit:                opts.Limit,
-		Continue:             opts.Continue,
-		SendInitialEvents:    opts.SendInitialEvents,
-	}
+	metaOpts := convertListOptsToKube(opts)
+
 	return rc.client.AppsV1().Deployments(namespace).List(ctx, metaOpts)
 }
 
 func (rc *DeploymentInterfaces) Delete(ctx context.Context, namespace, name string, opts model.DeleteOptions) error {
-	gracePeriodSeconds := new(int64)
-	if opts.GracePeriodSeconds != nil {
-		gracePeriodSeconds = opts.GracePeriodSeconds
-	}
-
-	preconditions := new(model.Preconditions)
-	if opts.Preconditions != nil {
-		preconditions = opts.Preconditions
-	}
-
-	metaOpts := metav1.DeleteOptions{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       opts.TypeMeta.Kind,
-			APIVersion: opts.TypeMeta.APIVersion,
-		},
-		GracePeriodSeconds: gracePeriodSeconds,
-		Preconditions: &metav1.Preconditions{
-			UID:             preconditions.UID,
-			ResourceVersion: preconditions.ResourceVersion,
-		},
-		DryRun: opts.DryRun,
-	}
+	metaOpts := convertDeleteOptsToKube(opts)
 
 	return rc.client.AppsV1().Deployments(namespace).Delete(ctx, name, metaOpts)
 }
