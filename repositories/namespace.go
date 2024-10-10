e29bfc4ffaae2e9f0939ec346a2c3af0983ca0e2
fleimkeipa <adem_sahin12@hotmail.com>
2024-10-11 01:43:37 +0300

    Update namespace options

--- repositories/namespace.go
+++ repositories/namespace.go
@@ -6,7 +6,6 @@ import (
 	"github.com/fleimkeipa/kubernetes-api/model"
 
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -21,73 +20,25 @@ func NewNamespaceRepository(client *kubernetes.Clientset) *NamespaceRepository {
 }
 
 func (rc *NamespaceRepository) Create(ctx context.Context, namespace *corev1.Namespace, opts model.CreateOptions) (*corev1.Namespace, error) {
-	metaOpts := metav1.CreateOptions{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       opts.TypeMeta.Kind,
-			APIVersion: opts.TypeMeta.APIVersion,
-		},
-		DryRun:          opts.DryRun,
-		FieldManager:    opts.FieldManager,
-		FieldValidation: opts.FieldValidation,
-	}
+	metaOpts := convertCreateOptsToKube(opts)
+
 	return rc.client.CoreV1().Namespaces().Create(ctx, namespace, metaOpts)
 }
 
 func (rc *NamespaceRepository) Update(ctx context.Context, namespace *corev1.Namespace, opts model.UpdateOptions) (*corev1.Namespace, error) {
-	metaOpts := metav1.UpdateOptions{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       opts.TypeMeta.Kind,
-			APIVersion: opts.TypeMeta.APIVersion,
-		},
-		DryRun:          opts.DryRun,
-		FieldManager:    opts.FieldManager,
-		FieldValidation: opts.FieldValidation,
-	}
+	metaOpts := convertUpdateOptsToKube(opts)
+
 	return rc.client.CoreV1().Namespaces().Update(ctx, namespace, metaOpts)
 }
 
 func (rc *NamespaceRepository) List(ctx context.Context, opts model.ListOptions) (*corev1.NamespaceList, error) {
-	metaOpts := metav1.ListOptions{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       opts.TypeMeta.Kind,
-			APIVersion: opts.TypeMeta.APIVersion,
-		},
-		LabelSelector:        opts.LabelSelector,
-		FieldSelector:        opts.FieldSelector,
-		Watch:                opts.Watch,
-		AllowWatchBookmarks:  opts.AllowWatchBookmarks,
-		ResourceVersion:      opts.ResourceVersion,
-		ResourceVersionMatch: metav1.ResourceVersionMatch(opts.ResourceVersionMatch),
-		TimeoutSeconds:       opts.TimeoutSeconds,
-		Limit:                opts.Limit,
-		Continue:             opts.Continue,
-		SendInitialEvents:    opts.SendInitialEvents,
-	}
+	metaOpts := convertListOptsToKube(opts)
+
 	return rc.client.CoreV1().Namespaces().List(ctx, metaOpts)
 }
 
 func (rc *NamespaceRepository) Delete(ctx context.Context, name string, opts model.DeleteOptions) error {
-	gracePeriodSeconds := new(int64)
-	if opts.GracePeriodSeconds != nil {
-		gracePeriodSeconds = opts.GracePeriodSeconds
-	}
+	metaOpts := convertDeleteOptsToKube(opts)
 
-	preconditions := new(model.Preconditions)
-	if opts.Preconditions != nil {
-		preconditions = opts.Preconditions
-	}
-
-	metaOpts := metav1.DeleteOptions{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       opts.TypeMeta.Kind,
-			APIVersion: opts.TypeMeta.APIVersion,
-		},
-		GracePeriodSeconds: gracePeriodSeconds,
-		Preconditions: &metav1.Preconditions{
-			UID:             preconditions.UID,
-			ResourceVersion: preconditions.ResourceVersion,
-		},
-		DryRun: opts.DryRun,
-	}
 	return rc.client.CoreV1().Namespaces().Delete(ctx, name, metaOpts)
 }
